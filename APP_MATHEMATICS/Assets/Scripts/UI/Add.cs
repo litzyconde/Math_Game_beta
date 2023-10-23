27777ab6e6ba1020c6391a5f49236e41fc6b8ef0
YamilUchani <yamilronalduchaniguachalla@gmail.com>
2023-10-22 22:09:53 -0400

    Yamil Uchani
    Se añadio el efecto de un boton para aumentar el valor de un elemento.

---
+++ APP_MATHEMATICS/Assets/Scripts/UI/Add.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Add : MonoBehaviour
+{
+    public int valor=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void adding()
+    {
+        valor++;
+
+    }
+}
