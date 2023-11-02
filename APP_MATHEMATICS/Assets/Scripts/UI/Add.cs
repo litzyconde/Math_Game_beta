3033f9e7feea74ed80b1536dc1b780184e60ccdc
YamilUchani <yamilronalduchaniguachalla@gmail.com>
2023-11-02 10:49:10 -0400

    Yamil Uchani
    Correcciones

--- APP_MATHEMATICS/Assets/Scripts/UI/Add.cs
+++ APP_MATHEMATICS/Assets/Scripts/UI/Add.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using System;
 public class Add : MonoBehaviour
 {
-    public int valor=0;
+    public TMP_Text numbertext;
+    public int cont;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    private void Update() {
+        numbertext.text = cont.ToString();
+    }
     // Update is called once per frame
     public void adding()
     {
-        valor++;
-
+        cont++;
     }
 }
